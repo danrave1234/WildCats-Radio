ef60fe4d057b20ea78fb8aded32b2c98dd955d38
Danrave Cabahug Keh <danravekeh123@gmail.com>
2025-05-08 08:24:44 +0800

    Revert "vite config"
    This reverts commit 8efcb375a7661028057715ed30b21eb2012ed1e2.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,8 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        // target: 'https://wildcat-radio-f05d362144e6.herokuapp.com',
-        target: 'https://wildcat-radio-f05d362144e6.autoidleapp.com',
+        target: 'https://wildcat-radio-f05d362144e6.herokuapp.com',
         changeOrigin: true,
         secure: true
       }
