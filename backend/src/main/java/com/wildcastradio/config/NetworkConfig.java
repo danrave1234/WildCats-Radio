68199c0e2b7669aadde7f3910ac4fc5f362dc773
bean_sint <123951763+beansint@users.noreply.github.com>
2025-05-24 13:51:30 +0800

    NetworkConfig fix Postconstruct

--- backend/src/main/java/com/wildcastradio/config/NetworkConfig.java
+++ backend/src/main/java/com/wildcastradio/config/NetworkConfig.java
@@ -5,7 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import javax.annotation.PostConstruct;
+import jakarta.annotation.PostConstruct;
 import java.net.InetAddress;
 import java.net.NetworkInterface;
 import java.util.Enumeration;
