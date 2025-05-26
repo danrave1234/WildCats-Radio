c70cb0dfcfe4b4983289a7cc3aadd7dd71f9521c
danrave1234 <danravekeh123@gmail.com>
2025-05-26 22:34:32 +0800

    Enhance CORS configuration by adding additional allowed origins in application properties and security settings

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'wildcat-radio-f05d362144e6.autoidleapp.com',
+        target: 'https://wildcat-radio-f05d362144e6.autoidleapp.com',
         changeOrigin: true,
         secure: true
       }
