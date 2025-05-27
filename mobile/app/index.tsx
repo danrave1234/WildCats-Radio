c1039c1e595a11319c795b6f54821edb00a62e91
drewqt11 <andriapas4@gmail.com>
2025-05-27 10:27:33 +0800

    delete old mobile

--- mobile/app/index.tsx
+++
@@ -1,7 +0,0 @@
-import { Redirect } from 'expo-router';
-import React from 'react';
-
-export default function IndexScreen() {
-  // Redirect to the welcome screen
-  return <Redirect href="/welcome" />;
-} 
